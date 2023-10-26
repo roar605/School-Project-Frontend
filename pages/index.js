163f4c35ff26d0bc6024886305bd4940830b5ab1
sidhart25 <navneetnishchal1420@gmail.com>
2023-10-26 23:23:33 +0530

    basic redux store and reducer setup is done

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Navbar from "@/app/components/navbar/Navbar";
 import Home from "@/app/components/home/Home";
 import Footer from "@/app/components/footer/Footer";
+import {store} from '../redux/store';
+import { Provider } from 'react-redux'
 
 export default function Index() {
     return (
       <>
+      <Provider store={store}>
         <Navbar />
         <Home />
         <Footer/>
+      </Provider>
+       
       </>
     );
   }
